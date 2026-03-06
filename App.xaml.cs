a437eb083ec8e1d2fff7ab51eb78b1d275f268d9
Raygunpewpew1 <raygunpewpew@gmail.com>
2026-03-05 21:38:02 -0600

    image picker

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,6 +14,9 @@ public partial class App : Application
         _serviceProvider = serviceProvider;
     }
 
+    /// <summary>Service provider for controls that need to resolve services (e.g. image loading).</summary>
+    public static IServiceProvider? ServiceProvider => (Current as App)?._serviceProvider;
+
     protected override Window CreateWindow(IActivationState? activationState)
     {
         var loadingPage = _serviceProvider.GetRequiredService<LoadingPage>();
